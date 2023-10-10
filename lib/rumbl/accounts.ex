0166dcde6e12136f26979200e220bc525ae4aa98
Gabrielparizet <parizetgabriel@gmail.com>
2023-10-10 18:15:17 +0200

    feat(accounts/user): Create database table for users and methods to fetch them in accounts.ex

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -2,24 +2,23 @@ defmodule Rumbl.Accounts do
   @moduledoc """
   The Accounts context.
   """
-
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
-  def list_users do
-    [
-      %User{id: "1", name: "José", username: "josevalim"},
-      %User{id: "2", name: "Bruce", username: "redrapids"},
-      %User{id: "3", name: "Chris", username: "chrismaccord"}
-    ]
+  def get_user(id) do
+    Repo.get(User, id)
   end
 
-  def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
   end
+
+  def list_users do
+    Repo.all(User)
+  end
+
 end
