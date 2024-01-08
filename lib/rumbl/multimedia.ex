6ab1716c387d97c66b0a967156ee3fc3c6bcf37f
Gabrielparizet <parizetgabriel@gmail.com>
2024-01-08 18:27:50 +0100

    feat(Multimedia): Created schema and migration for categories

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -4,8 +4,9 @@ defmodule Rumbl.Multimedia do
   """
 
   import Ecto.Query, warn: false
-  alias Rumbl.Repo
 
+  alias Rumbl.Accounts
+  alias Rumbl.Repo
   alias Rumbl.Multimedia.Video
 
   @doc """
@@ -19,6 +20,12 @@ defmodule Rumbl.Multimedia do
   """
   def list_videos do
     Repo.all(Video)
+  end
+
+  def list_user_videos(%Accounts.User{} = user) do
+    Video
+    |> user_videos_query(user)
+    |> Repo.all()
   end
 
   @doc """
@@ -37,6 +44,12 @@ defmodule Rumbl.Multimedia do
   """
   def get_video!(id), do: Repo.get!(Video, id)
 
+  def get_user_video!(%Accounts.User{} = user, id) do
+    Video
+    |> user_videos_query(user)
+    |> Repo.get!(id)
+  end
+
   @doc """
   Creates a video.
 
@@ -49,9 +62,10 @@ defmodule Rumbl.Multimedia do
       {:error, %Ecto.Changeset{}}
 
   """
-  def create_video(attrs \\ %{}) do
+  def create_video(%Accounts.User{} = user, attrs \\ %{}) do
     %Video{}
     |> Video.changeset(attrs)
+    |> Ecto.Changeset.put_assoc(:user, user)
     |> Repo.insert()
   end
 
@@ -100,5 +114,9 @@ defmodule Rumbl.Multimedia do
   """
   def change_video(%Video{} = video, attrs \\ %{}) do
     Video.changeset(video, attrs)
+  end
+
+  defp user_videos_query(query, %Accounts.User{id: user_id}) do
+    from(v in query, where: v.user_id == ^user_id)
   end
 end
--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -7,6 +7,7 @@ defmodule Rumbl.Multimedia.Video do
     field :title, :string
     field :url, :string
     belongs_to :user, Rumbl.Accounts.User
+    belongs_to :category, Rumbl.Multimedia.Category
 
     timestamps()
   end
@@ -14,7 +15,7 @@ defmodule Rumbl.Multimedia.Video do
   @doc false
   def changeset(video, attrs) do
     video
-    |> cast(attrs, [:url, :title, :description])
+    |> cast(attrs, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
   end
 end
