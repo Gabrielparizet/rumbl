2ffe7f5add834ca1c569a50ad9320427d10dee93
Gabrielparizet <parizetgabriel@gmail.com>
2024-01-30 17:36:08 +0100

    feat(multimedia): associated videos and categories

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -125,4 +125,11 @@ defmodule Rumbl.Multimedia do
   def create_category!(name) do
     Repo.insert!(%Category{name: name}, on_conflict: :nothing)
   end
+
+
+  def list_alphabetical_categories do
+    Category
+    |> Category.alphabetical()
+    |> Repo.all()
+  end
 end
